374617153212b93a64eb0cc4591fedd24b7d6a0f
IVÁN CARRERAS PÉREZ <icarreras19961@iesjoanramis.org>
2022-10-28 16:44:20 +0200

    Ejercicio 16 acabado

--- src/Ejercicios/Ex4_16.java
+++ src/Ejercicios/Ex4_16.java
@@ -11,18 +11,18 @@ public class Ex4_16 {
 		String residuo="";
 		/*********///Calcuadora del binario
 		do {
-			numUser=numUser/2;
 			residuo=residuo + Integer.toString(numUser%2);
-		}while(numUser>=2);
+			numUser=numUser/2;
+		}while(numUser>1);
 		/********///Escribe el primer dijito solo si es 1
 		if(numUser==1) {
-			System.out.print(numUser);
+			System.out.print('1');
 		}
 		/********///escribira el resultado de las operaciones al reves para que el numero salga bien
-		for(int cont=residuo.length();cont>0;cont--) {
-			int num = residuo.charAt(cont);
-			System.out.print(residuo);
+		for(int cont=residuo.length()-1;cont>=0;cont--) {
+			System.out.print(residuo.charAt(cont));
 		}
+		
 	/* Se puede hacer asi 
 	   String binario = Integer.toBinaryString(numUser);
 	   System.out.println(binario);
